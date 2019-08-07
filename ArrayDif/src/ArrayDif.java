4ac8ef2174838d31c14283b9aafd3844ebf2efa4
Ostroverhov, Vladislav <v.ostroverhov@a1qa.com>
2019-08-07 15:31:41 +0300

    start

--- ArrayDif/src/ArrayDif.java
+++ ArrayDif/src/ArrayDif.java
@@ -1,2 +1,15 @@
 public class ArrayDif {
+    public static void main(String[] args) {
+        String[] arr1 = {"Alex", "Dima", "Kate", "Galina", "Ivan"};
+        String[] arr2 = {"Dima", "Ivan", "Kate"};
+
+        int i,j;
+        String[] newArr = new String[0];
+
+        for (i = 0; i < arr1.length; i++);
+        System.out.println(arr1[i]);
+            for (j = 0; j < arr2.length; j++);
+                if (arr1[i].equals(arr2[j]));
+                else String[] resultArr;
+    }
 }
