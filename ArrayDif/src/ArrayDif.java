9b1c84fd008cf8a7e9de547ec2a97a0766bc3a5e
Ostroverhov, Vladislav <v.ostroverhov@a1qa.com>
2019-08-07 17:12:04 +0300

    Array difference

--- ArrayDif/src/ArrayDif.java
+++ ArrayDif/src/ArrayDif.java
@@ -1,15 +1,34 @@
+import java.util.Arrays;
+
 public class ArrayDif {
     public static void main(String[] args) {
         String[] arr1 = {"Alex", "Dima", "Kate", "Galina", "Ivan"};
         String[] arr2 = {"Dima", "Ivan", "Kate"};
 
-        int i,j;
-        String[] newArr = new String[0];
+        int counter = 0;
 
-        for (i = 0; i < arr1.length; i++);
-        System.out.println(arr1[i]);
-            for (j = 0; j < arr2.length; j++);
-                if (arr1[i].equals(arr2[j]));
-                else String[] resultArr;
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i].equals(arr2[j])){
+                    counter++;
+                }
+            }
+        }
+        int newLenth = arr1.length - counter;
+        String[] newArr = new String[newLenth];
+
+        System.out.println(newLenth);
+
+        for (int k = 0; k<newLenth; k++){
+            for (int y = 0; y < arr1.length; y++) {
+                for (int x = 0; x < arr2.length; x++) {
+                    if (arr1[y].equals(arr2[x]));
+                    else {
+                        newArr[k] = arr1[y];
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(newArr));
     }
 }
