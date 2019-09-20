260443d90a9066da3b67a40ed2d7627aa1dbad13
Ostroverhov, Vladislav <v.ostroverhov@a1qa.com>
2019-09-20 12:29:15 +0300

    tas4 with corrections v.2

--- task4/src/main/java/app/pages/CarPage.java
+++ task4/src/main/java/app/pages/CarPage.java
@@ -2,10 +2,15 @@ package app.pages;
 
 import app.form.InfoSection;
 import app.form.TopMenu;
+import org.openqa.selenium.By;
 
-public class CarPage {
+public class CarPage extends BasePage {
     private InfoSection infoSection = new InfoSection();
     private TopMenu topMenu = new TopMenu();
+
+    public CarPage() {
+        super("Car Page", By.xpath("//td[@class='mmy-spec__compare']//a"));
+    }
 
     public InfoSection getInfoSection() {
         return infoSection;
